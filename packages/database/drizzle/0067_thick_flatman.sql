41a60bf94b46e2a340f1794543d868fe04057043
Jeremy <jeremy@metadao.fi>
2026-01-05 09:54:08 -0800

    dropped duplicate constraint

--- packages/database/drizzle/0067_thick_flatman.sql
+++ packages/database/drizzle/0067_thick_flatman.sql
@@ -30,5 +30,3 @@ CREATE INDEX IF NOT EXISTS "risk_history_address_index" ON "address_risk_assessm
 CREATE INDEX IF NOT EXISTS "risk_history_assessed_at_index" ON "address_risk_assessment_history" USING btree ("assessed_at");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "address_risk_index" ON "address_risk_assessments" USING btree ("risk");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "address_type_index" ON "address_risk_assessments" USING btree ("address_type");--> statement-breakpoint
-ALTER TABLE "v0_6_conditional_swaps" ADD CONSTRAINT "v0_6_conditional_swaps_signature_unique" UNIQUE("signature");--> statement-breakpoint
-ALTER TABLE "v0_6_spot_swaps" ADD CONSTRAINT "v0_6_spot_swaps_signature_unique" UNIQUE("signature");
